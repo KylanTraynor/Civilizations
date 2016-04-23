584f2deae9ff8cbd114547ddad44920bf4e99ba2
KylanTraynor <kylan2837@gmail.com>
2016-04-23 21:43:56 +0200

    Stuff.

--- Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/WebListener.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/WebListener.java
@@ -31,8 +31,7 @@ public class WebListener implements Listener{
 	    		if(target.equals("/")){
 	    			target = "/Index.jsp";
 	    			try {
-	    				ServletContext sc = baseReq.getServletContext();
-	    				Civilizations.log("WARNING", "Current Path: " + sc.getContextPath());
+	    				ServletContext sc = baseReq.getContext();
 						sc.getRequestDispatcher(target).forward(req, res);
 					} catch (ServletException | IOException e1) {
 						// TODO Auto-generated catch block
