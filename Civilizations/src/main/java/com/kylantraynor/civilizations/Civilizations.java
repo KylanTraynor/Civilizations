3e5b719a116979d9e8de860c4e5e3ce454b14ec5
KylanTraynor <kylan2837@gmail.com>
2016-04-24 04:11:36 +0200

    ...

--- Civilizations/src/main/java/com/kylantraynor/civilizations/Civilizations.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/Civilizations.java
@@ -162,11 +162,17 @@ public class Civilizations extends JavaPlugin{
 		
 		setupCommands();
 		
-		startWebServer();
+		int port = 8120;
+		try {
+			startWebServer(port);
+			log("INFO", "Successfully started webserver on port " + port);
+		} catch (Exception e) {
+			log("WARNING", "Could not start webserver on port " + port + ". This port is probably already in use.");
+			e.printStackTrace();
+		}
 	}
 
-	private void startWebServer() throws Exception{
-		int port = 8120;
+	private void startWebServer(int port) throws Exception{
 		Server server = new Server(port);
 		ServletHandler handler = new ServletHandler();
 		server.setHandler(handler);
