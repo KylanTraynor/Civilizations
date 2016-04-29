be2b7fd83ff48cc817e0eb61e0f2f36da0de2fc7
KylanTraynor <kylan2837@gmail.com>
2016-04-29 15:22:05 +0200

    Test

--- Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/ChatListener.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/ChatListener.java
@@ -1,14 +1,38 @@
 package com.kylantraynor.civilizations.listeners;
 
+import mkremins.fanciful.FancyMessage;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import com.kylantraynor.civilizations.groups.House;
 
 public class ChatListener implements Listener{
 	
 	@EventHandler
 	public void onAsyncPlayerChat(AsyncPlayerChatEvent event){
-		event.setFormat("%s: %s");
+		String format = "%s%house: %s";
+		
+		if(event.getPlayer() != null){
+			House h = House.get(event.getPlayer());
+			if(h != null){
+				format = format.replace("%house", " " + h.getName());
+			} else {
+				format = format.replace("%house", "");
+			}
+		}
+		
+		FancyMessage fm = new FancyMessage();
+		fm.then("%s");
+		if(House.get(event.getPlayer()) != null){
+			fm.then(" " + House.get(event.getPlayer()).getName());
+			fm.tooltip("Test");
+		}
+		fm.then(": ");
+		fm.then("%s");
+		
+		event.setFormat(fm.toJSONString());
 	}
 	
 }
