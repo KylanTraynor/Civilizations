171e8107b402a6be6739448d416e66621f1d0278
KylanTraynor <kylan2837@gmail.com>
2016-05-06 16:41:45 +0200

    reformated the chat display.

--- Civilizations/src/main/java/com/kylantraynor/civilizations/chat/ChatTools.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/chat/ChatTools.java
@@ -9,7 +9,7 @@ public class ChatTools {
 	public static String formatTitle(String title, ChatColor green){
 		if(title == null) title = "";
 		if(green == null) green = ChatColor.GRAY;
-		String line = getDelimiter();
+		String line = "" + delimiter;
 		int pivot = line.length() / 2;
 		String center = ".[ " + green + title + ChatColor.GRAY + " ].";
 		String out = ChatColor.GRAY + line.substring(0, Math.max(0, (pivot - center.length() / 2)));
