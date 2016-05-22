b99c21b91b2e81fb224ad132884f21471ebaa2b4
KylanTraynor <kylan2837@gmail.com>
2016-05-22 17:10:27 +0200

    Updated boats.

--- Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/VehiclesListener.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/VehiclesListener.java
@@ -3,19 +3,21 @@ package com.kylantraynor.civilizations.listeners;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.spigotmc.event.entity.EntityMountEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VehiclesListener implements Listener{
 	
 	@EventHandler
-	public void onEntityMount(EntityMountEvent event){
+	public void onEntityInteract(PlayerInteractEntityEvent event){
 		if(event.isCancelled()) return;
-		if(event.getMount().getType() == EntityType.BOAT){
-			double distance = event.getMount().getLocation().distance(event.getEntity().getLocation());
-			if(distance < 1){
-				
-			} else {
-				event.setCancelled(true);
+		if(event.getRightClicked() != null){
+			if(event.getRightClicked().getType() == EntityType.BOAT){
+				double distance = event.getRightClicked().getLocation().distance(event.getPlayer().getLocation());
+				if(distance < 1.5){
+					
+				} else {
+					event.setCancelled(true);
+				}
 			}
 		}
 	}
