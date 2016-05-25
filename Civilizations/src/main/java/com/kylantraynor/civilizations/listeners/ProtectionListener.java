e342717ed413800ecaf3cbb88af269143ad9a585
KylanTraynor <kylan2837@gmail.com>
2016-05-25 18:48:19 +0200

    Protects Villagers in MarketStalls

--- Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/ProtectionListener.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/listeners/ProtectionListener.java
@@ -1,19 +1,25 @@
 package com.kylantraynor.civilizations.listeners;
 
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.kylantraynor.civilizations.Cache;
 import com.kylantraynor.civilizations.Civilizations;
 import com.kylantraynor.civilizations.groups.settlements.Settlement;
+import com.kylantraynor.civilizations.groups.settlements.plots.Plot;
 import com.kylantraynor.civilizations.groups.settlements.plots.market.MarketStall;
 import com.kylantraynor.civilizations.protection.LockManager;
+import com.kylantraynor.civilizations.protection.PermissionType;
 
 public class ProtectionListener implements Listener{
 	
@@ -86,6 +92,21 @@ public class ProtectionListener implements Listener{
 							}
 						}
 					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onEntityDamageByEntity(EntityDamageByEntityEvent event){
+		if(event.isCancelled()) return;
+		if(event.getEntityType() == EntityType.VILLAGER){
+			Plot p = Plot.getAt(event.getEntity().getLocation());
+			if(p instanceof MarketStall && event.getDamager() instanceof Player){
+				if(((MarketStall)p).isOwner((OfflinePlayer) event.getDamager())){
+					
+				} else {
+					event.setCancelled(true);
 				}
 			}
 		}
