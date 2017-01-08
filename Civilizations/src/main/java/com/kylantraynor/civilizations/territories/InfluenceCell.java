67a298a5fbb7087f5a3fa159c865fafdd39aaae3
KylanTraynor <kylan2837@gmail.com>
2017-01-08 14:55:06 +0100

    Changed InfluenceCell constructor

--- Civilizations/src/main/java/com/kylantraynor/civilizations/territories/InfluenceCell.java
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/territories/InfluenceCell.java
@@ -1,13 +1,10 @@
 package com.kylantraynor.civilizations.territories;
 
 import com.kylantraynor.voronoi.VCell;
-import com.kylantraynor.voronoi.VSite;
-import com.kylantraynor.voronoi.Voronoi;
 
 public class InfluenceCell extends VCell {
 
-	public InfluenceCell(VSite site, Voronoi<InfluenceCell> v) {
-		super(site, v);
+	public InfluenceCell() {
 	}
 
 }
