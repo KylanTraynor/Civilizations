322f98acd05a88133258b4d49b9c8ced1d04b8e7
KylanTraynor <kylan2837@gmail.com>
2017-02-10 19:34:21 +0100

    Stupid me.

---
+++ Civilizations/src/main/java/com/kylantraynor/civilizations/hook/towny/TownyTownProtection.java
@@ -0,0 +1,35 @@
+package com.kylantraynor.civilizations.hook.towny;
+
+import com.kylantraynor.civilizations.groups.settlements.plots.Plot;
+import com.kylantraynor.civilizations.protection.SettlementProtection;
+import com.kylantraynor.civilizations.shapes.Hull;
+import com.kylantraynor.civilizations.shapes.Shape;
+
+public class TownyTownProtection extends SettlementProtection {
+
+	public TownyTownProtection(TownyTown town) {
+		super(town);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Hull getHull(){
+		if(hull == null) hull = new Hull(getSettlement().getLocation());
+		if(this.hullNeedsUpdate){
+			hull.clear();
+			for(Plot p : getSettlement().getPlots()){
+				for(Shape s : p.getProtection().getShapes()){
+					hull.addPoints(s.getVertices());
+				}
+			}
+			for(Plot p : ((TownyTown)getSettlement()).getTownyPlots()){
+				for(Shape s : p.getProtection().getShapes()){
+					hull.addPoints(s.getVertices());
+				}
+			}
+			hull.updateHull();
+		}
+		return hull;
+	}
+
+}
